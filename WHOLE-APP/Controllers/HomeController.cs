ae5f4f662aabb378a9fddb8a8d9d8fb9aae5686e
Steven Serrao <stevenserraowork@gmail.com>
2024-09-06 09:13:42 +0200

    Initial version of ASP.NET weather app of Udemy

---
+++ WHOLE-APP/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Weather_App_1.Models;
+
+namespace Weather_App_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("/weather")]
+        public IActionResult AllCities()
+        {
+            List<CityWeather> cities = new List<CityWeather>
+            {
+               new CityWeather { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"), TemperatureCelsius = 1 },
+               new CityWeather { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"), TemperatureCelsius = 16 },
+               new CityWeather { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"), TemperatureCelsius = 28 }
+            };
+
+            // Sending viewbag assignment of cities for dynamic assignment of "AllCities.cshtml"
+            ViewBag.Cities = cities;
+
+            return View();
+        }
+
+        [Route("/weather/{cityCode}")]
+        public IActionResult City(string? cityCode)
+        {
+            List<CityWeather> cities = new List<CityWeather>
+            {
+               new CityWeather { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"), TemperatureCelsius = 1 },
+               new CityWeather { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"), TemperatureCelsius = 16 },
+               new CityWeather { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"), TemperatureCelsius = 28 }
+            };
+
+            CityWeather? ciadad = cities.FirstOrDefault(c => c.CityUniqueCode == cityCode);
+
+            // Sending MODEL so that "City.cshtml" view contains strongly typed version
+            return View(ciadad);
+        }
+    }
+}
---
+++ WHOLE-APP/Models/CityWeather.cs
@@ -0,0 +1,11 @@
+﻿namespace Weather_App_1.Models
+{
+    public class CityWeather
+    {
+        public string? CityUniqueCode { get; set; }
+        public string? CityName { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int TemperatureCelsius { get; set; }
+
+    }
+}
